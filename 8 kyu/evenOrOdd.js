3c04079c4f960f9f6851db3062ed91f2d3c19cab
tlek <tikenty@gmail.com>
2023-09-02 14:00:33 +0600

    added test cases

--- 8 kyu/evenOrOdd.js
+++ 8 kyu/evenOrOdd.js
@@ -5,3 +5,27 @@ function even_or_odd(number) {
 }
 
 console.log(even_or_odd(2), "Even")
+
+/* TEST CASES
+const chai = require('chai');
+const assert = chai.assert;
+
+describe("Sample tests",() => {
+
+  it("2 is even", () => {
+    assert.strictEqual(evenOrOdd(2), "Even");
+  });
+  it("7 is odd", () => {
+    assert.strictEqual(evenOrOdd(7), "Odd");
+  });
+  it("-42 is even", () => {
+    assert.strictEqual(evenOrOdd(-42), "Even");
+  });
+  it("-7 is odd", () => {
+    assert.strictEqual(evenOrOdd(-7), "Odd");
+  });
+  it("0 is even", () => {
+    assert.strictEqual(evenOrOdd(0), "Even");
+  });
+});
+ */
