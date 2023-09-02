d3a0248720d7eec97076ccb4da7ecd0722469848
tlek <tikenty@gmail.com>
2023-09-03 00:00:12 +0600

    added task and solution for booleanToString.js

---
+++ 8 kyu/booleanToString.js
@@ -0,0 +1,16 @@
+/* DESCRIPTION
+Implement a function which convert the given boolean value into its string representation.
+
+Note: Only valid inputs will be given.
+ */
+
+/* ✅ SOLUTION */
+
+function booleanToString(b) {
+    return b.toString();
+    /* return b ? 'true' : 'false'; */
+    /* return b + ''; */
+    /* return `${b}`; */
+    /* return String(b); */
+    /* return b ? 'true' : 'false'; */
+}
