ad8f1be729651e5b858581956613f021f892f989
tlek <tikenty@gmail.com>
2023-09-03 17:57:08 +0600

    added task and solution for #reversed strings

---
+++ 8 kyu/reversedStrings.js
@@ -0,0 +1,12 @@
+/* DESCRIPTION Reversed Strings
+Complete the solution so that it reverses the string passed into it.
+
+'world'  =>  'dlrow'
+'word'   =>  'drow'
+ */
+
+/* ✅ SOLUTION */
+
+function solution(str) {
+    return str.split('').reverse().join('');
+}
