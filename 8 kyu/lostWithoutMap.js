ff6dc025c8e96cbc1ca99a97f5c8237607ee7f3f
tlek <tikenty@gmail.com>
2023-09-18 22:05:42 +0600

    added solution for 8 kyu task: lostWithoutMap.js

---
+++ 8 kyu/lostWithoutMap.js
@@ -0,0 +1,15 @@
+/* ❓ DESCRIPTION
+Given an array of integers, return a new array with each value doubled.
+
+For example:
+
+[1, 2, 3] --> [2, 4, 6]
+ */
+
+function maps(x) {
+    const doubledArray = [];
+    for (let i = 0; i < x.length; i++) {
+        doubledArray.push(x[i] * 2);
+    }
+    return doubledArray;
+}
